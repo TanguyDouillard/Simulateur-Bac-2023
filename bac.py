af1585d47911573c77a77a1d8de76f6763c4ad28
alex-eth <86966117+alex-eth@users.noreply.github.com>
2023-04-19 08:38:10 +0200

    Update bac.py

--- bac.py
+++ bac.py
@@ -1,2 +1,25 @@
 
+from tkinter import *
+from PIL import Image, ImageTk
+import sqlite3
 
+connection = sqlite3.connect("BdD.db")
+cursor = connection.cursor()
+
+
+cursor.execute("CREATE TABLE IF NOT EXISTS contact(id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE, prenom TEXT, nom TXT, numero INT, adresse TXT, email TXT, date TXT)")
+
+fenetre = Tk()
+fenetre.title("Simulateur des notes du Bac")
+
+
+fenetre.geometry("1000x800")
+fenetre.configure(bg = "SteelBlue3")
+
+
+
+
+
+
+
+fenetre.mainloop()
