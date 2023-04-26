2de739bb785fb2869890a65fbca8684cf97869a1
alex-eth <86966117+alex-eth@users.noreply.github.com>
2023-04-26 10:19:57 +0200

    Update Simulateur_bac_version_pas_finie.py

--- Simulateur_bac_version_pas_finie.py
+++ Simulateur_bac_version_pas_finie.py
@@ -16,6 +16,38 @@ fenetre.geometry("1920x1000")
 fenetre.configure(bg = "grey8")
 
 
+def mention():
+##    moy=moyenne(Prenom)
+    moy = 16
+    titre= Label(fenetre, text = moy, fg = 'light coral', bg="grey14")
+    titre.configure(font=("Source Serif Pro Semibold",20))
+    titre.place(x=620, y=604)
+
+    if moy>=16:
+        titre= Label(fenetre, text = " Mention très bien ! ",fg = 'light coral', bg="grey14")
+        titre.configure(font=("Source Serif Pro Semibold",20))
+        titre.place(x=965, y=300)
+
+    elif moy>=14:
+        titre= Label(fenetre, text = " Mention bien ! ",fg = 'light coral', bg="grey14")
+        titre.configure(font=("Source Serif Pro Semibold",20))
+        titre.place(x=987, y=300)
+
+    elif moy>=12:
+        titre= Label(fenetre, text = " Mention assez bien ! ",fg = 'light coral', bg="grey14")
+        titre.configure(font=("Source Serif Pro Semibold",20))
+        titre.place(x=950, y=300)
+
+    elif moy>=10:
+        titre= Label(fenetre, text = " Pas de mentions ",fg = 'light coral', bg="grey14")
+        titre.configure(font=("Source Serif Pro Semibold",20))
+        titre.place(x=975, y=300)
+
+    else :
+        titre= Label(fenetre, text = " Tu n'as pas eu ton bac :-( ",fg = 'light coral', bg="grey14")
+        titre.configure(font=("Source Serif Pro Semibold",20))
+        titre.place(x=920, y=300)
+
 canevas = Canvas(fenetre, width=450, height=500, bg="grey12",  highlightbackground = "grey10")
 canevas.pack(padx=5,pady=5)
 canevas.place(x=400, y=200)
@@ -30,15 +62,11 @@ titre.place(x=90, y=55)
 
 titre= Label(fenetre, text = "Résultat",fg = 'black')
 titre.configure(font=("Source Serif Pro Semibold",25))
-titre.place(x=1000, y=55)
+titre.place(x=1008, y=55)
 
-titre= Label(fenetre, text = "Mention : ",fg = 'dark violet', bg="grey8")
+titre= Label(fenetre, text = " Mention : ",fg = 'dark violet', bg="grey14")
 titre.configure(font=("Source Serif Pro Semibold",25))
 titre.place(x=1000, y=235)
-
-titre= Label(fenetre, text = "Moyenne : ",fg = 'dark violet', bg="grey8")
-titre.configure(font=("Source Serif Pro Semibold",25))
-titre.place(x=995, y=435)
 
 bouton_1 = Button(fenetre,text = " Alexis ",bg = 'blue violet',command = fenetre.destroy)
 bouton_1.configure(height=4, width=25)
@@ -76,5 +104,47 @@ bouton_quit = Button(fenetre,text = " Quit ",bg = 'firebrick2',command = fenetre
 bouton_quit.configure(height=4, width=25)
 bouton_quit.place(x=1050,y=850)
 
-fenetre.mainloop()
+#################################################################
+#################################################################
 
+titre= Label(fenetre, text = " Ecrit de Français : ",fg = 'darkOrchid2', bg="grey14")
+titre.configure(font=("Source Serif Pro Semibold",15))
+titre.place(x=435, y=240)
+
+titre= Label(fenetre, text = " Oral de Français : ",fg = 'darkOrchid2', bg="grey14")
+titre.configure(font=("Source Serif Pro Semibold",15))
+titre.place(x=435, y=300)
+
+titre= Label(fenetre, text = " SPE 1 : ",fg = 'darkOrchid2', bg="grey14")
+titre.configure(font=("Source Serif Pro Semibold",15))
+titre.place(x=435, y=360)
+
+titre= Label(fenetre, text = " SPE 2 : ",fg = 'darkOrchid2', bg="grey14")
+titre.configure(font=("Source Serif Pro Semibold",15))
+titre.place(x=435, y=420)
+
+titre= Label(fenetre, text = " Philosophie : ",fg = 'darkOrchid2', bg="grey14")
+titre.configure(font=("Source Serif Pro Semibold",15))
+titre.place(x=435, y=480)
+var_Philo = StringVar()
+saisie_Philo = Entry(fenetre,textvariable = var_Philo , width =5)
+saisie_Philo.place(x = 569 , y = 488)
+
+titre= Label(fenetre, text = " Grand Oral : ",fg = 'darkOrchid2', bg="grey14")
+titre.configure(font=("Source Serif Pro Semibold",15))
+titre.place(x=435, y=540)
+var_GO = StringVar()
+saisie_GO = Entry(fenetre,textvariable = var_GO , width =5)
+saisie_GO.place(x = 565 , y = 546)
+
+bouton_9 = Button(fenetre,text = " ✓ ",bg = 'green yellow',command = mention)
+bouton_9.configure(height=2, width=7)
+bouton_9.place(x=755,y=502)
+
+titre= Label(fenetre, text = " Moyenne : ",fg = 'dark violet', bg="grey14")
+titre.configure(font=("Source Serif Pro Semibold",25))
+titre.place(x=435, y=600)
+
+#################################################################
+#################################################################
+fenetre.mainloop()
